a9fdaa7944816db66a299a9355054c70e269f013
Matthias Rudin <matthias.rudin@live.com>
2018-11-07 04:43:02 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+def display_board(board_array)
+  puts " #{board_array[0]} | #{board_array[1]} | #{board_array[2]} "
+  puts "-----------"
+  puts " #{board_array[3]} | #{board_array[4]} | #{board_array[5]} "
+  puts "-----------"
+  puts " #{board_array[6]} | #{board_array[7]} | #{board_array[8]} "
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || "O"
+    return true
+  end
+end
